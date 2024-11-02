f27640c25ae0b06e0b90958f283a085f4c2be17b
VikingRobotics2984 <ljhsvikingrobotics@gmail.com>
2024-11-02 12:16:01 -0700

    Changing to new code
    Deleting old, and using formatting to fix new

--- Robot.java
+++ Robot.java
@@ -1,63 +1,15 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+// This is a simple Java program.
+// FileName : "HelloWorld.java".
 
-package frc.robot;
-
-import edu.wpi.first.util.sendable.SendableRegistry;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.drive.MecanumDrive;
-import com.ctre.phoenix6.hardware.TalonFX;
-import edu.wpi.first.wpilibj.motorcontrol.MotorController;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
-
-
-/** This is a demo program showing how to use Mecanum control with the MecanumDrive class. */
-public class Robot extends TimedRobot {
-
-
-  private MecanumDrive m_robotDrive ;
-  private Joystick stick;
-
-  @Override
-  public void robotInit() {
-    TalonFX frontLeft = new TalonFX(ConstantID.kFrontLeftChannel);
-    TalonFX rearLeft = new TalonFX(ConstantID.kRearLeftChannel);
-    TalonFX frontRight = new TalonFX(ConstantID.kFrontRightChannel);
-    TalonFX rearRight = new TalonFX(ConstantID.kRearRightChannel);
-    rearRight.setInverted(false);
-    SendableRegistry.addChild(m_robotDrive, frontLeft);
-    SendableRegistry.addChild(m_robotDrive, rearLeft);
-    SendableRegistry.addChild(m_robotDrive, frontRight);
-    SendableRegistry.addChild(m_robotDrive, rearRight);
-
-    // Invert the right side motors.
-    // You may need to change or remove this to match your robot.
-    //frontRight.setInverted(true);
-    
-
-    m_robotDrive = new MecanumDrive(frontLeft::set, rearLeft::set, frontRight::set, rearRight::set);
-
-    stick = new Joystick(ConstantID.kJoystickChannel);
-    // motor = new PWMSparkMax(13);
-
-
-  }
-
-  @Override
-  public void teleopPeriodic() {
-    // Use the joystick Y axis for forward movement, X axis for lateral
-    // movement, and Z axis for rotation.
-    double speedMultiplier = 0.5;
-    m_robotDrive.driveCartesian(-stick.getY()*-1*speedMultiplier, -stick.getZ()*speedMultiplier, -stick.getX()*speedMultiplier);
-    
-    // frontRight.setInverted(true);
-    // rearRight.setInverted(true);
-    // frontLeft.set(.1);
-    // rearLeft.set(.1);
-    // frontRight.set(.1);
-    // rearRight.set(.1);
-
-  }
+class Main {
+	// Your program begins with a call to main().
+	// Prints "Hello, World" to the terminal window.
+	// public static void main(String args[])
+	// {
+	// 	System.out.println("Hello, World");
+	// }
+	WPI_TalonFX left_front = new WPI_TalonFX(FRONT_LEFT_ID);
+    WPI_TalonFX right_front = new WPI_TalonFX(FRONT_RIGHT_ID);
+    WPI_TalonFX left_back = new WPI_TalonFX(BACK_LEFT_ID);
+    WPI_TalonFX right_back = new WPI_TalonFX(BACK_RIGHT_ID);
 }
---
+++ utils/ConstantID.java
@@ -0,0 +1,10 @@
+package frc.robot;
+
+public class  ConstantID{
+    public static final int kFrontLeftChannel = 1;
+    public static final int kRearLeftChannel = 3;
+    public static final int kFrontRightChannel = 2;
+    public static final int kRearRightChannel = 4;
+    
+    public static final int kJoystickChannel = 0;
+}
